5b14e5aa40f70cf07d714df875e3df576d5b7ffb
Andr0human <ayushsinha132001@gmail.com>
2025-04-18 07:09:07 +0000

    added resignation and draw offer

--- client/app/modals/index.js
+++ client/app/modals/index.js
@@ -1,6 +1,13 @@
+import DrawOfferModal from "./DrawOfferModal";
 import GameOverModal from "./GameOverModal";
 import GameStatusModal from "./GameStatusModal";
 import PromotionModal from "./PromotionModal";
 import StartOptionModal from "./StartOptionModal";
 
-export { GameOverModal, GameStatusModal, PromotionModal, StartOptionModal };
+export {
+  DrawOfferModal,
+  GameOverModal,
+  GameStatusModal,
+  PromotionModal,
+  StartOptionModal,
+};
