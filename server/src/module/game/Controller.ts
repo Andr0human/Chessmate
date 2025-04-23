05315220d0e5a4b09bd1bb10b3aae49ce449c986
Andr0human <ayushsinha132001@gmail.com>
2025-04-23 11:31:22 +0000

    updated disconnection logic for singleplayer

--- server/src/module/game/Controller.ts
+++ server/src/module/game/Controller.ts
@@ -1,7 +1,8 @@
 import { Request, Response } from "express";
 import logger from "../../lib/logger";
 import { SystemResponse } from "../../lib/response-handler";
-import { checkGameRoomExists } from "./helpers";
+import { IRoom } from "./entities";
+import { checkGameRoomExists, getAllGameRooms } from "./helpers";
 
 class GameController {
   static checkRoomAvailability = (req: Request, res: Response): void => {
@@ -14,6 +15,20 @@ class GameController {
       logger.error("Error checking room availability", error);
 
       new SystemResponse(res, "Error checking room availability", {
+        error,
+      }).internalServerError();
+    }
+  };
+
+  static getAll = (req: Request, res: Response): void => {
+    try {
+      const rooms: IRoom[] = getAllGameRooms();
+
+      new SystemResponse(res, "All rooms fetched", { rooms }).ok();
+    } catch (error: unknown) {
+      logger.error("Error getting all rooms", error);
+
+      new SystemResponse(res, "Error getting all rooms", {
         error,
       }).internalServerError();
     }
