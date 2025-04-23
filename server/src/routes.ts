38b32c4af8d533fc0cc2de4960b112a999ea6a5d
Andr0human <ayushsinha132001@gmail.com>
2025-04-23 12:37:34 +0000

    Added speedtest api for engine

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,13 +1,15 @@
 import { Router } from "express";
 import { HealthController } from "./controllers";
 import { ErrorHandlerMiddleware } from "./lib/middlewares";
-import { gameRouter } from "./module/game";
+import { chessEngineRouter, gameRouter } from "./module";
 
 const router: Router = Router();
 
 router.get("/health", HealthController.check);
 
 router.use("/api/game", gameRouter);
+
+router.use("/api/engine", chessEngineRouter);
 
 // Handles '404 not found'
 router.use(ErrorHandlerMiddleware.notFound);
